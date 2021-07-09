4be3e9d47ea7437ac1a7d9f437d8f27d0c9e8340
abhirup-kabiraj <80841918+abhirup-kabiraj@users.noreply.github.com>
2021-07-09 16:28:53 +0530

    Update app.js v2

--- App.js
+++ App.js
@@ -1,50 +1,67 @@
-import  React, { useState } from 'react'
-import Editor from "./Editor"
 
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from './useLocalStorage'
 function App() {
-const [html, sethtml] = useState('')
-const [css, setcss] = useState('')
-const [javascript, setjs] = useState('')
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+  const [py, setpy] = useLocalStorage('python', '')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+          <script>${py}</script>
+        </html>
+      `)
+    }, 250)
+    return () => clearTimeout(timeout)
+  }, [html, css, js,py])
 
-
-const srcDoc=
-  <html>
-    <body>{html}</body>
-    <style>{css}</style>
-    <script>{javascript}</script>
-  </html>
- return (
+  return (
     <>
-    <div className="pane top-pane">
-    <Editor 
-    displayName="HTML" 
-    language="xml"
-    value={html}
-    onChange={sethtml}
-    />
-    <Editor 
-    displayName="CSS" 
-    language="css"
-    value={css}
-    onChange={setcss}/>
-    <Editor 
-    displayName="JAVASCRIPT" 
-    language="javascript"
-    value={javascript}
-    onChange={setjs}/>
-    </div>
-    <div className="pane">
-    <iframe
-    srcDoc={srcDoc}
-    title="output"
-    sandbox="allow-scripts"
-    width ="100%"
-    height="100%"></iframe>
-    </div>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+         <Editor
+          language="python"
+          displayName="PYTHON"
+          value={py}
+          onChange={setpy}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </>
-  );
-
+  )
 }
-
 
 export default App;
